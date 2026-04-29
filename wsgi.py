d343d998f2fa9b4b3e901fc34d7144110456011e
Tushar <tusharmks9816@gmail.com>
2026-04-29 13:48:41 +0530

    fix wsgi path for render

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from run import app
+import sys, os
+sys.path.insert(0, '/opt/render/project/src')
+os.chdir('/opt/render/project/src')
+from backend.app import create_app, socketio
+app = create_app()
