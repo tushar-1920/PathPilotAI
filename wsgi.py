bbfd313a8b53c9c9c8fadfcac36369edeb3ba1c8
Tushar <tusharmks9816@gmail.com>
2026-04-29 13:54:42 +0530

    fix wsgi.py app export

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
-import sys, os
-sys.path.insert(0, '/opt/render/project/src')
-os.chdir('/opt/render/project/src')
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from backend.app import create_app, socketio
+
 app = create_app()
